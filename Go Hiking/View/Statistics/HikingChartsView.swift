8a4e923de11fa699e08dc648fe43024865c67888
Decemberease <68339620+Decemberease@users.noreply.github.com>
2024-04-22 14:25:03 +0800

    Add files via upload

--- Go Hiking/View/Statistics/HikingChartsView.swift
+++ Go Hiking/View/Statistics/HikingChartsView.swift
@@ -10,38 +10,38 @@ import CoreData
 
 struct HikingChartsView: View {
     var invalidData: Bool
-    @FetchRequest var bikeRidesIn1Week: FetchedResults<BikeRide>
-    @FetchRequest var bikeRidesIn1WeekBefore: FetchedResults<BikeRide>
-    @FetchRequest var bikeRidesIn5Weeks: FetchedResults<BikeRide>
-    @FetchRequest var bikeRidesIn5WeeksBefore: FetchedResults<BikeRide>
-    @FetchRequest var bikeRidesIn26Weeks: FetchedResults<BikeRide>
-    @FetchRequest var bikeRidesIn26WeeksBefore: FetchedResults<BikeRide>
+    @FetchRequest var HikingsIn1Week: FetchedResults<Hiking>
+    @FetchRequest var HikingsIn1WeekBefore: FetchedResults<Hiking>
+    @FetchRequest var HikingsIn5Weeks: FetchedResults<Hiking>
+    @FetchRequest var HikingsIn5WeeksBefore: FetchedResults<Hiking>
+    @FetchRequest var HikingsIn26Weeks: FetchedResults<Hiking>
+    @FetchRequest var HikingsIn26WeeksBefore: FetchedResults<Hiking>
 
     init() {
         self.invalidData = false
         
-        let requests: [NSFetchRequest<BikeRide>?] = BikeRide.fetchRequestsWithDateRanges()
+        let requests: [NSFetchRequest<Hiking>?] = Hiking.fetchRequestsWithDateRanges()
         let countNils = requests.filter({ $0 == nil }).count
         // Use default fetch requests if any of the date range requests failed
         if (countNils > 0 && requests.count == 6) {
             self.invalidData = true
-            let request: NSFetchRequest<BikeRide> = BikeRide.fetchRequest()
+            let request: NSFetchRequest<Hiking> = Hiking.fetchRequest()
             request.sortDescriptors = []
-            self._bikeRidesIn1Week = FetchRequest<BikeRide>(fetchRequest: request)
-            self._bikeRidesIn1WeekBefore = FetchRequest<BikeRide>(fetchRequest: request)
-            self._bikeRidesIn5Weeks = FetchRequest<BikeRide>(fetchRequest: request)
-            self._bikeRidesIn5WeeksBefore = FetchRequest<BikeRide>(fetchRequest: request)
-            self._bikeRidesIn26Weeks = FetchRequest<BikeRide>(fetchRequest: request)
-            self._bikeRidesIn26WeeksBefore = FetchRequest<BikeRide>(fetchRequest: request)
+            self._HikingsIn1Week = FetchRequest<Hiking>(fetchRequest: request)
+            self._HikingsIn1WeekBefore = FetchRequest<Hiking>(fetchRequest: request)
+            self._HikingsIn5Weeks = FetchRequest<Hiking>(fetchRequest: request)
+            self._HikingsIn5WeeksBefore = FetchRequest<Hiking>(fetchRequest: request)
+            self._HikingsIn26Weeks = FetchRequest<Hiking>(fetchRequest: request)
+            self._HikingsIn26WeeksBefore = FetchRequest<Hiking>(fetchRequest: request)
         }
         // Otherwise, use the correct date range requests - it is safe to force unwrap here
         else {
-            self._bikeRidesIn1Week = FetchRequest<BikeRide>(fetchRequest: requests[0]!)
-            self._bikeRidesIn1WeekBefore = FetchRequest<BikeRide>(fetchRequest: requests[1]!)
-            self._bikeRidesIn5Weeks = FetchRequest<BikeRide>(fetchRequest: requests[2]!)
-            self._bikeRidesIn5WeeksBefore = FetchRequest<BikeRide>(fetchRequest: requests[3]!)
-            self._bikeRidesIn26Weeks = FetchRequest<BikeRide>(fetchRequest: requests[4]!)
-            self._bikeRidesIn26WeeksBefore = FetchRequest<BikeRide>(fetchRequest: requests[5]!)
+            self._HikingsIn1Week = FetchRequest<Hiking>(fetchRequest: requests[0]!)
+            self._HikingsIn1WeekBefore = FetchRequest<Hiking>(fetchRequest: requests[1]!)
+            self._HikingsIn5Weeks = FetchRequest<Hiking>(fetchRequest: requests[2]!)
+            self._HikingsIn5WeeksBefore = FetchRequest<Hiking>(fetchRequest: requests[3]!)
+            self._HikingsIn26Weeks = FetchRequest<Hiking>(fetchRequest: requests[4]!)
+            self._HikingsIn26WeeksBefore = FetchRequest<Hiking>(fetchRequest: requests[5]!)
         }
     }
     
@@ -66,26 +66,26 @@ struct HikingChartsView: View {
     func getDistanceData(index: Int) -> [Double] {
         var result: [Double] = [0.0, 0.0]
         if (index == 0) {
-            for entry in bikeRidesIn1WeekBefore {
+            for entry in HikingsIn1WeekBefore {
                 result[0] += entry.hikingDistance
             }
-            for entry in bikeRidesIn1Week {
+            for entry in HikingsIn1Week {
                 result[1] += entry.hikingDistance
             }
         }
         if (index == 1) {
-            for entry in bikeRidesIn5WeeksBefore {
+            for entry in HikingsIn5WeeksBefore {
                 result[0] += entry.hikingDistance
             }
-            for entry in bikeRidesIn5Weeks {
+            for entry in HikingsIn5Weeks {
                 result[1] += entry.hikingDistance
             }
         }
         if (index == 2) {
-            for entry in bikeRidesIn26WeeksBefore {
+            for entry in HikingsIn26WeeksBefore {
                 result[0] += entry.hikingDistance
             }
-            for entry in bikeRidesIn26Weeks {
+            for entry in HikingsIn26Weeks {
                 result[1] += entry.hikingDistance
             }
         }
@@ -95,26 +95,26 @@ struct HikingChartsView: View {
     func getTimeData(index: Int) -> [Double] {
         var result: [Double] = [0.0, 0.0]
         if (index == 0) {
-            for entry in bikeRidesIn1WeekBefore {
+            for entry in HikingsIn1WeekBefore {
                 result[0] += entry.hikingTime
             }
-            for entry in bikeRidesIn1Week {
+            for entry in HikingsIn1Week {
                 result[1] += entry.hikingTime
             }
         }
         if (index == 1) {
-            for entry in bikeRidesIn5WeeksBefore {
+            for entry in HikingsIn5WeeksBefore {
                 result[0] += entry.hikingTime
             }
-            for entry in bikeRidesIn5Weeks {
+            for entry in HikingsIn5Weeks {
                 result[1] += entry.hikingTime
             }
         }
         if (index == 2) {
-            for entry in bikeRidesIn26WeeksBefore {
+            for entry in HikingsIn26WeeksBefore {
                 result[0] += entry.hikingTime
             }
-            for entry in bikeRidesIn26Weeks {
+            for entry in HikingsIn26Weeks {
                 result[1] += entry.hikingTime
             }
         }
@@ -123,13 +123,13 @@ struct HikingChartsView: View {
     
     func getNumberOfRoutes(index: Int) -> [Int] {
         if (index == 0) {
-            return [bikeRidesIn1WeekBefore.count, bikeRidesIn1Week.count]
+            return [HikingsIn1WeekBefore.count, HikingsIn1Week.count]
         }
         if (index == 1) {
-            return [bikeRidesIn5WeeksBefore.count, bikeRidesIn5Weeks.count]
+            return [HikingsIn5WeeksBefore.count, HikingsIn5Weeks.count]
         }
         if (index == 2) {
-            return [bikeRidesIn26WeeksBefore.count, bikeRidesIn26Weeks.count]
+            return [HikingsIn26WeeksBefore.count, HikingsIn26Weeks.count]
         }
         return [0, 0]
     }
